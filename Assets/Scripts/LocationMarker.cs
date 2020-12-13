e53c456551416e9dc844a218898d6a5988313481
RorySan <games@rory.es>
2020-12-13 19:21:39 +0100

    Files renamed

---
+++ Assets/Scripts/LocationMarker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class GizmoSphere : MonoBehaviour
+{
+    [SerializeField] private string locationName;
+    [SerializeField] private string locationDescription;
+    [SerializeField] private PointOfInterest pointOfInterest;
+    
+    public string GetLocationName()
+    {
+        return locationName;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        pointOfInterest.HasPlayer = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        pointOfInterest.HasPlayer = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+}
